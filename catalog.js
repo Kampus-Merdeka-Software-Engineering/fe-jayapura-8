7b090427f67b541d03b5abfb296a8460def77022
Gilang Alfitra <gilangalfitra5@gmail.com>
2023-09-26 22:08:20 +0700

    update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,32 +1,88 @@
-// Memilih semua elemen dengan kelas 'filter-product' (item produk) dan 'filter-btn' (tombol filter)
-const allFilterItems = document.querySelectorAll('.filter-product');
-const allFilterBtns = document.querySelectorAll('.filter-btn');
+// Ketika halaman web dimuat
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleksi elemen HTML dengan class "filter-products"
+  const productsContainer = document.querySelector(".filter-products");
 
-// Menambahkan event listener untuk memastikan tombol pertama aktif saat halaman dimuat
-window.addEventListener('DOMContentLoaded', () => {
-  allFilterBtns[0].classList.add('active-btn');
-});
+  // Data produk dalam bentuk array objek
+  const productsData = [
+    {
+      id: 1,
+      name: "Anggur",
+      imageSrc: "img/anggur.png",
+      price: "$3.99",
+      stars: 5,
+      categories: ["all"],
+    },
+    {
+      id: 2,
+      name: "Fresh Orange",
+      imageSrc: "img/jeruk.png",
+      price: "$3.99",
+      stars: 5,
+      categories: ["all", "new"],
+    },
+    {
+      id: 3,
+      name: "Watermelon",
+      imageSrc: "img/semangka.png",
+      price: "$3.99",
+      stars: 4.5,
+      categories: ["all", "new", "best-sellers"],
+    },
+    {
+      id: 4,
+      name: "Salak",
+      imageSrc: "img/salak.png",
+      price: "$4.99",
+      stars: 4,
+      categories: ["all", "new", "best-sellers", "specials"],
+    }
+  ];
 
-// Menambahkan event listener untuk setiap tombol filter
-allFilterBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-      showFilteredContent(btn);
-  });
-});
+  // Fungsi untuk membuat HTML untuk satu produk
+  function createProduct(product) {
+    const productHTML = `
+      <div class="filter-product ${product.categories.join(" ")}">
+        <img src="${product.imageSrc}" alt="${product.name}">
+        <h3>${product.name}</h3>
+        <div class="price">${product.price}</div>
+        <div class="stars">
+          ${"<i class='fas fa-star'></i>".repeat(Math.floor(product.stars))}
+          ${product.stars % 1 !== 0 ? "<i class='fas fa-star-half-alt'></i>" : ""}
+        </div>
+      </div>
+    `;
+    return productHTML;
+  }
 
-// Fungsi untuk menampilkan konten yang sesuai dengan tombol filter yang diklik
-function showFilteredContent(btn){
-  allFilterItems.forEach((item) => {
-    // Memeriksa apakah elemen produk memiliki kelas yang sesuai dengan tombol filter yang diklik
-      if(item.classList.contains(btn.id)){
-          resetActiveBtn(); // Mereset status aktif pada semua tombol filter
-          btn.classList.add('active-btn'); // Menandai tombol filter yang aktif
-          item.style.display = "block"; // Menampilkan elemen produk
-      } else {
-          item.style.display = "none"; // Menyembunyikan elemen produk yang tidak sesuai
+  // Fungsi untuk menampilkan produk berdasarkan kategori yang dipilih
+  function displayProducts(category) {
+    productsContainer.innerHTML = ""; // Kosongkan container produk
+
+    // Loop melalui data produk
+    productsData.forEach((product) => {
+      // Tampilkan produk jika kategori sesuai dengan yang dipilih atau "all"
+      if (category === "all" || product.categories.includes(category)) {
+        const productHTML = createProduct(product);
+        productsContainer.innerHTML += productHTML;
       }
+    });
+  }
+
+  // Seleksi semua tombol filter dengan class "filter-btn"
+  const filterButtons = document.querySelectorAll(".filter-btn");
+
+  // Tambahkan event listener untuk setiap tombol filter
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const filterId = button.id; // Dapatkan ID tombol yang diklik
+      displayProducts(filterId); // Tampilkan produk berdasarkan kategori yang dipilih
+    });
   });
-}
+
+  // Tampilkan semua produk saat halaman pertama kali dimuat
+  displayProducts("all");
+});
 
 // Fungsi untuk mereset status aktif pada semua tombol filter
 function resetActiveBtn(){
